92b3ae6d5286da0bf312b5d1e06d635448d4d936
jleeson <jleeson2000@gmail.com>
2020-03-30 11:10:21 -0700

    deprecated Component.onclick

--- src/components/Component.js
+++ src/components/Component.js
@@ -32,9 +32,6 @@ export class Component extends HTMLElement {
             this.shadowRoot.appendChild(element);
         }
         this.didLoad();
-
-        /* register the onclick response */
-        this.shadowRoot.addEventListener("click", this.onclick.bind(this));
     }
 
     /**
@@ -95,6 +92,7 @@ export class Component extends HTMLElement {
      * Add a click event listener to the component.
      * @param {MouseEvent} event - The click event that occured.
      * @abstract
+     * @deprecated onlick should not be used anymore and instead custom event listeners should be assigned in the didLoad function.
      */
     onclick() { }
 
